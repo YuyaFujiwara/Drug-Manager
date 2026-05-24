f7469db4930871343311ad8c1e961a804202398f
unknown <ariadastmotonobu@gmail.com>
2026-05-24 15:12:04 +0900

    alart

--- app/src/main/java/com/example/drugmanage/MainActivity.kt
+++ app/src/main/java/com/example/drugmanage/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.drugmanage
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import com.example.drugmanage.theme.DrugManageTheme
 
@@ -16,6 +21,17 @@ class MainActivity : ComponentActivity() {
 
     enableEdgeToEdge()
     setContent {
+      val launcher = rememberLauncherForActivityResult(
+          contract = ActivityResultContracts.RequestPermission(),
+          onResult = { }
+      )
+
+      LaunchedEffect(Unit) {
+          if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+              launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+          }
+      }
+
       DrugManageTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
     }
   }
